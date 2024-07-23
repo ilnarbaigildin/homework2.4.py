18c1c98902a4395f6ab86e20fe8de2877b03b9be
ilnarbaigildin <baga2012@mail.ru>
2024-07-24 02:02:45 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+my_string = input('Привет! ')
+количество = int(input('Введите количество символов введенного текста: '))
+print('Ваш ответ:', количество)
+
+name = input('Напишите слово My string! ')
+print(name.upper())
+print(name.lower())
+print(name.replace(' ', ''))
+print(name[0])
+print(name[-1])
