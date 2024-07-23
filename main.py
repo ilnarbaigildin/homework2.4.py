0fd1584efdaa2c7347ec986d32f6d0773b8724cc
ilnarbaigildin <baga2012@mail.ru>
2024-07-24 02:31:18 +0500

    Update main.py
    ниже сделал правильнее

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ print(name.lower())
 print(name.replace(' ', ''))
 print(name[0])
 print(name[-1])
+
+
+my_string = input('Привет! ')
+str_my_string = str(my_string)
+length = len(str_my_string)
+print(length)
+
+name = input('Напишите слово My string! ')
+print(name.upper())
+print(name.lower())
+print(name.replace(' ', ''))
+print(name[0])
+print(name[-1])
